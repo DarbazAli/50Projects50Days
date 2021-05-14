c2039930d2a02745d6d1fb5361ad6cbcbfd6644a
Darbaz Ali <darbaz.me@gmail.com>
2021-05-14 15:44:02 +0300

    #39 - Image Password Strength - change color based on strength

--- 39_Image_Password_Strength/index.js
+++ 39_Image_Password_Strength/index.js
@@ -11,10 +11,21 @@ password.addEventListener('input', (e) => {
   background.style.filter = `blur(${blurValue}px)`
 
   let p_strength_value = len * 5
+  let bgcolor = 'red'
 
   if (p_strength_value >= 100) {
     p_strength_value = 100
   }
 
+  console.log(len)
+  if (len >= 15) {
+    bgcolor = 'green'
+  } else if (len >= 8) {
+    bgcolor = 'orange'
+  } else {
+    bgcolor = 'red'
+  }
+
   p_strength.style.width = `${p_strength_value}%`
+  p_strength.style.backgroundColor = bgcolor
 })
