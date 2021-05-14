a3d57a8452a037c348923a5e6cb9f83216358fd1
Darbaz Ali <darbaz.me@gmail.com>
2021-05-14 11:52:23 +0300

    #39 - Image Password Strength - Stength Indicator

--- 39_Image_Password_Strength/index.js
+++ 39_Image_Password_Strength/index.js
@@ -2,9 +2,19 @@
 
 const password = document.getElementById('password')
 const background = document.getElementById('background')
+const p_strength = document.getElementById('password-strength')
 
 password.addEventListener('input', (e) => {
   const input = e.target.value
-  const blurValue = 20 - input.length * 2
+  const len = input.length
+  const blurValue = 20 - len * 2
   background.style.filter = `blur(${blurValue}px)`
+
+  let p_strength_value = len * 5
+
+  if (p_strength_value >= 100) {
+    p_strength_value = 100
+  }
+
+  p_strength.style.width = `${p_strength_value}%`
 })
